0d2c8d5ea4f8cb1452785c48f101df003ef927e6
Yolanda <mathselse@gmail.com>
2021-04-10 17:57:29 -0400

    remove activity outliers

--- data_cleaning_activity.py
+++ data_cleaning_activity.py
@@ -5,18 +5,18 @@ import numpy as np
 import sys
 from matplotlib import pyplot as plt
 
-activity = ['Activity_1.xlsx', 'Activity_2.xlsx']
+activity = [ 'Activity_2.xlsx']
 columns = ['cow_id', 'activity', 'model', 'observation_time', 'server_time', 'local_id']
 
 def data_cleaning(file_dict, f_name, save):
     df = pd.read_excel(file_dict + f_name, index_col=0, engine='openpyxl')
     size = len(df)
-    # df = df.drop(labels=['Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9'])
-    df = df.dropna(subset=['activity'])
+    df['activity'] = df[df['activity'] < 1000]
+    df.dropna(subset=['activity'], inplace=True)
     print(f'Clean file {f_name}, size={len(df)}, original size={size}')
     plt.title(f_name)
-    df[['activity']].boxplot()
-    plt.savefig('./pic/' + f_name.replace('.xlsx', '') + '.jpg')
+    df[['activity']].plot.box()
+    plt.savefig('./pic/' + f_name.replace('.xlsx', '_cl') + '.jpg')
     plt.show()
     if save:
         df.to_csv(file_dict + '/project_clean/')
